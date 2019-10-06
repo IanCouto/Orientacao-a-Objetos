201ba5946b9e3e6a3ecefb7c8ecdc7cc933b78ea
Gam3MasAlvaro <Gam3MasAlvaro@LAPTOP-2NJHNNHC>
2019-10-06 15:50:51 -0300

    Alvaro

--- trabalho/src/main/java/com/mycompany/trabalho/Main.java
+++ trabalho/src/main/java/com/mycompany/trabalho/Main.java
@@ -13,5 +13,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Teste");
         System.out.println("Augusto");
+        System.out.println("Alvaro");
     }
 }
