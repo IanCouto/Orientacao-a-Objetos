4057411ba80fa7426a0c1bf08a7f7a6844340d9e
IanCouto <48738352+IanCouto@users.noreply.github.com>
2019-10-06 16:30:59 -0300

    inicia classes

--- trabalho/src/main/java/com/mycompany/trabalho/Estoque.java
+++ trabalho/src/main/java/com/mycompany/trabalho/Estoque.java
@@ -4,11 +4,40 @@
  * and open the template in the editor.
  */
 package com.mycompany.trabalho;
+import java.util.ArrayList;
 
 /**
  *
  * @author icout
  */
 public class Estoque {
+    private ArrayList<Produto> produtos;
+    private ArrayList<Produto> lixo;
+    Estoque(Produto produto){
+        this.produtos = new ArrayList();
+    }
     
+    void adicionaProduto(Produto produto){
+        produtos.add(produto);
+    }
+    void removeProdutoNome(String nome){
+        for(int i = 0; i < produtos.size(); i++ ){
+            if(produtos.get(i).getNome().equals(nome)){
+                lixo.add(produtos.get(i));
+                produtos.remove(i);
+            }
+        }
+    }
+    void removeProdutoId(Integer id){
+        for(int i = 0; i < produtos.size(); i++ ){
+            if(produtos.get(i).getId() == id){
+                lixo.add(produtos.get(i));
+                produtos.remove(i);
+            }
+        }
+    }
+    
+    void limpaLixo(){
+        lixo.clear();
+    }
 }
--- trabalho/src/main/java/com/mycompany/trabalho/Produto.java
+++ trabalho/src/main/java/com/mycompany/trabalho/Produto.java
@@ -10,5 +10,36 @@ package com.mycompany.trabalho;
  * @author icout
  */
 public class Produto {
+    private String nome;
+    private Integer id;
+    private Integer quantidade;
+    private Float valor;
     
+    Produto(String nome, int id, int quantidade, float valor){
+        this.nome = nome;
+        this.id = id;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+    Produto(){
+        this.nome = new String();
+        this.id = null;
+        this.quantidade = null;
+        this.valor = null; 
+    }
+    Integer getId(){
+        return id;
+    }
+    
+    String getNome(){
+        return nome;
+    }
+    
+    Integer getQuantidade(){
+        return quantidade;
+    }
+    
+    Float getValor(){
+        return valor;
+    }
 }
